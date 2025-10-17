b432dc5304aef2a5f8a56837b4200b2f06cbc6a0
LH-and-FPGA <180024497+LH-and-FPGA@users.noreply.github.com>
2025-10-17 12:00:23 +0100

    Task1-1

--- task1/counter_tb.cpp
+++ task1/counter_tb.cpp
@@ -21,13 +21,19 @@ int main(int argc, char **argv, char **env) {
     top->en = 0;
 
     for (i = 0; i < 300; i++) {
+        top->rst = (i < 2) | (i == 15);
+        top->en = (i > 4);
+        // If I put it here, I can get the en at high at i=5, which is what I expect
         for (clk = 0; clk < 2; clk++) {
             tfp->dump(2*i+clk);
             top->clk = !top->clk;
             top->eval();
         }
-        top->rst = (i < 2) | (i == 15);
-        top->en = (i > 4);
+        // top->rst = (i < 2) | (i == 15);
+        // top->en = (i > 4);
+        // top->eval();
+        // If I put the eval here, it seems that this eval won't work.
+        // As I can still get the same wavefont.
         if (Verilated::gotFinish()) exit(0);
     }
     tfp->close();
